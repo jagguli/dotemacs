468aac91d40ecbceb46cf003753d66ab3e586a60
Steven Joseph <steven@stevenjoseph.in>
2013-11-28 23:46:58 +1100

    added function to add an adhoc note

--- packs/core/config/orgmode-config.el
+++ packs/core/config/orgmode-config.el
@@ -9,5 +9,13 @@
                              "~/Dropbox/OrgMode/startup.org" 
                              "~/Dropbox/OrgMode/zen.org"))
 
+(defun note ()
+  (interactive)
+  (find-file "~/Dropbox/OrgMode/notes.org")
+  (goto-char (point-min))
+  (insert "------------------------------------------------------------\n")
+  (esk-insert-date)
+  (insert "\n\n")
+ (previous-line) )
 ;;(add-hook 'after-init-hook 'org-agenda-list)
 
