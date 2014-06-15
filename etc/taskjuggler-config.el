3379bed16dded11c30110edb4d22cc511e60fa28
Steven Joseph <steven@stevenjoseph.in>
2014-06-15 22:35:19 +1000

    added hook

--- etc/taskjuggler-config.el
+++ etc/taskjuggler-config.el
@@ -1,9 +1,10 @@
 (require 'taskjuggler-mode)
+(require 'hideshow)
 
 (add-hook 'taskjuggler-mode-hook 
           (lambda () 
             (hs-minor-mode)
             (hs-hide-all)
-            (define-key evil-normal-state-map "za" 'hs-show-all)
+            (define-key evil-normal-state-map (kbd "za") 'evil-toggle-fold)
             )
 )
