18f6d770a62e82a80a6f62c54b013e6fa14b1c2c
Steven Joseph <steven.joseph@iress.com.au>
2014-06-18 12:14:17 +1000

    added method to revert all buffers

--- etc/my-config.el
+++ etc/my-config.el
@@ -133,3 +133,18 @@ Chromium."
 )
 
 (ad-activate 'replace-highlight)
+
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files
+    http://blog.plover.com/prog/revert-all.html"
+  (interactive)
+  (let* ((list (buffer-list))
+         (buffer (car list)))
+    (while buffer
+      (when (and (buffer-file-name buffer) 
+                 (not (buffer-modified-p buffer)))
+        (set-buffer buffer)
+        (revert-buffer t t t))
+      (setq list (cdr list))
+      (setq buffer (car list))))
+  (message "Refreshed open files"))
