5327b9568b315b383cb1b03b05e121e9f51eb9d5
Steven Joseph <steven.joseph@iress.com.au>
2014-08-25 22:29:58 +1000

    fixed itail path expansion

--- etc/tail-config.el
+++ etc/tail-config.el
@@ -2,13 +2,13 @@
 (defun itail-xplan-server (branch)
   (interactive "nBranch: \n")
   (if (eq branch 0) (setq branch "") (setq branch (number-to-string branch)))
-  (itail (format "~/iress/xplan%s/var/local/log/server.log" branch))
+  (itail (expand-file-name (format "~/iress/xplan%s/var/local/log/server.log" branch)))
   )
 
 (defun itail-xplan-stderr (branch)
   (interactive "nBranch: \n")
   (if (eq branch 0) (setq branch "") (setq branch (number-to-string branch)))
-  (itail (format "~/iress/xplan%s/var/local/log/stderr.log" branch))
+  (itail (expand-file-name (format "~/iress/xplan%s/var/local/log/stderr.log" branch)))
   )
 
 (add-hook 'itail-mode-hook
