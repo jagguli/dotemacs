4a19c4fa061838658b7c76befc6bfd219d22a769
Steven Joseph <steven@stevenjoseph.in>
2014-08-31 12:40:55 +1000

    configured backup and history settings

--- emacs.el
+++ emacs.el
@@ -50,9 +50,21 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
 ;;(setq debug-on-error t)
-
+(setq savehist-additional-variables    ;; also save...
+  '(search-ring regexp-search-ring)    ;; ... my search entries
+  savehist-file (expand-file-name "~/share/Dropbox/emacs/history"))
+(savehist-mode t)         
 
   
-(setenv "SSH_AUTH_SOCK" (concat (getenv "HOME") "/.ssh-auth-sock"))
+;;(setenv "SSH_AUTH_SOCK" (concat (getenv "HOME") "/.ssh-auth-sock"))
 
 (setq plantuml-jar-path "/opt/plantuml/plantuml.jar")
+
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacs.d/tmp/backups"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
