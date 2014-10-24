c8b4d6e5052acdf51545b39385100b10d9d493e5
Steven Joseph <steven.joseph@iress.com.au>
2014-10-25 09:44:21 +1100

    configured mu4e with pass store

--- etc/mu4e-config.el
+++ etc/mu4e-config.el
@@ -1,3 +1,4 @@
+(require 'password-store)
 ;; these are actually the defaults
 ;; convert org mode to HTML automatically
 ;;(setq org-mu4e-convert-to-html nil)
@@ -6,7 +7,52 @@
 
 (defun email () 
   (interactive)
+  (setq mu4e-refile-folder
+        (lambda (msg)
+          (cond
+           ;; messages with football or soccer in the subject go to /football
+           ((string-match "football\\|soccer"
+                          (mu4e-message-field msg :subject))
+            "/football")
+           ;; messages sent by me go to the sent folder
+           ((find-if
+             (lambda (addr)
+               (mu4e-message-contact-field-matches msg :from addr))
+             mu4e-user-mail-address-list)
+            mu4e-sent-folder)
+           ;; everything else goes to /archive
+           ;; important to have a catch-all at the end!
+           (t  "/inbox"))))
+  ;; sending mail -- replace USERNAME with your gmail username
+  ;; also, make sure the gnutls command line utils are installed
+  ;; package 'gnutls-bin' in Debian/Ubuntu
+  
+  (require 'smtpmail)
+
+  ;; something about ourselves
+  (if (string-match "^.*.iress.com.au" system-name )
+      (progn
+        (setq
+         user-mail-address (password-store-get "iress/user")
+         user-full-name  "Steven Joseph"
+         message-signature nil
+         ;;message-send-mail-function 'smtpmail-send-it
+         message-send-mail-function 'message-send-mail-with-iress-sendmail
+         ;;smtpmail-stream-type 'plain
+         ;;smtpmail-default-smtp-server "localhost"
+         ;;smtpmail-smtp-server "localhost"
+         ;;smtpmail-smtp-service 1025)
+         )
+        )
+    (progn
+      (setq
+       user-mail-address "steven@stevenjoseph.in"
+       user-full-name  "steven@stevenjoseph.in"
+       message-signature nil)
+      )
+    )
  
+  
   (when (not (featurep 'mu4e))
     (add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e/")
  
@@ -16,12 +62,16 @@
     ;; defaults
  
     (setq
-     mu4e-maildir       "~/mail/"   ;; top-level Maildir
-     mu4e-sent-folder   "/Sent"       ;; folder for sent messages
-     mu4e-drafts-folder "/Drafts"     ;; unfinished messages
-     mu4e-trash-folder  "/Trash"      ;; trashed messages
-     mu4e-refile-folder "/Archive")   ;; saved messages
+     ;;mu4e-maildir       "~/.local/share/local-mail"   ;; top-level Maildir
+     mu4e-maildir       "~/mail/iress-local"   ;; top-level Maildir
+     mu4e-maildir       "/home/steven/.local/share/.local-mail.directory"   ;; top-level Maildir
+     mu4e-sent-folder   "/sent-mail"       ;; folder for sent messages
+     mu4e-drafts-folder "/drafts"     ;; unfinished messages
+     mu4e-trash-folder  "/trash"      ;; trashed messages
+     )
+     ;;mu4e-refile-folder "/archive")   ;; saved messages
  
+    
     ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
     (setq mu4e-sent-messages-behavior 'delete)
  
@@ -31,45 +81,16 @@
     ;; the 'All Mail' folder by pressing ``ma''.
  
     (setq mu4e-maildir-shortcuts
-          '( ("/INBOX"               . ?i)
-             ("/[Gmail].IMPORTANT"   . ?!)
+          '( ("/inbox"               . ?i)
+             ("/manup"   . ?u)
+             ("/me"   . ?m)
              ;; ("/gmail/[Gmail].Sent Mail"   . ?s)
              ;; ("/gmail/[Gmail].Trash"       . ?t)
-             ("/[Gmail].All Mail"    . ?a)))
+             ("/osc"    . ?o)))
  
     ;; allow for updating mail using 'U' in the main view:
     ;; I have this running in the background anyway
     ;; (setq mu4e-get-mail-command "offlineimap")
- 
-    ;; something about ourselves
-    (setq
-     user-mail-address "steven@stevenjoseph.in"
-     user-full-name  "steven@stevenjoseph.in"
-     message-signature nil)
- 
-    ;; sending mail -- replace USERNAME with your gmail username
-    ;; also, make sure the gnutls command line utils are installed
-    ;; package 'gnutls-bin' in Debian/Ubuntu
- 
-    (require 'smtpmail)
- 
-    (if (string< emacs-version "24")
-        (setq message-send-mail-function 'smtpmail-send-it
-              starttls-use-gnutls t
-              smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-              smtpmail-auth-credentials
-              '(("smtp.gmail.com" 587 "user@domain" nil))
-              smtpmail-default-smtp-server "smtp.gmail.com"
-              smtpmail-smtp-server "smtp.gmail.com"
-              smtpmail-smtp-service 587)
- 
-      ;; alternatively, for emacs-24 you can use:
-      (setq message-send-mail-function 'smtpmail-send-it
-            smtpmail-stream-type 'starttls
-            smtpmail-default-smtp-server "smtp.gmail.com"
-            smtpmail-smtp-server "smtp.gmail.com"
-            smtpmail-smtp-service 587)
-      )
  
  
     ;; don't keep message buffers around
@@ -104,7 +125,8 @@
     (setq org-mu4e-convert-to-html t)
  
     ;; need this to convert some e-mails properly
-    (setq mu4e-html2text-command "html2text -utf8 -width 72")
+    ;; (setq mu4e-html2text-command "html2text -utf8 -width 72")
+    (setq mu4e-html2text-command "html2text ")
   )
   (mu4e)
 )
