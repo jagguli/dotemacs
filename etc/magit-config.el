b352bd14b41c151fa19718724d30ad4a7dbc5d6d
Steven Joseph <steven.joseph@iress.com.au>
2014-10-25 09:24:51 +1100

    magit-gerrit configs

--- etc/magit-config.el
+++ etc/magit-config.el
@@ -1,1 +1,17 @@
 (global-set-key "\C-xgg" 'magit-status)
+
+(defun gerrit-check-if-repo-modified ()
+  "Check if current repo has been modified."
+  (null (mapcar (lambda (line)
+                       (string-match "^[ \t]+M" line))
+                     (magit-git-lines "status" "--porcelain -uno"))))
+
+
+(require 'magit-gerrit)
+
+;; if remote url is not using the default gerrit port and
+;; ssh scheme, need to manually set this variable
+(setq-default magit-gerrit-ssh-creds "steven.joseph@iress.com.au")
+
+;; if necessary, use an alternative remote instead of 'origin'
+(setq-default magit-gerrit-remote "gerrit")  
