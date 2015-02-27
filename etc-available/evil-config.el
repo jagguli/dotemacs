b974e63b64263c9eeac3388fa74a70414faa9eaf
Steven Joseph <steven.joseph@iress.com.au>
2015-02-27 21:19:55 +1100

    added colors for evil insert mode

--- etc-available/evil-config.el
+++ etc-available/evil-config.el
@@ -95,5 +95,17 @@
         (switch-to-buffer (other-buffer-ex))))
 
     (define-key evil-normal-state-map (kbd "C-^") 'evil-buffer-ex)
+    ;; change mode-line color by evil state
+    (lexical-let ((default-color (cons (face-background 'mode-line)
+                                       (face-foreground 'mode-line))))
+      (add-hook 'post-command-hook
+                (lambda ()
+                  (let ((color (cond ((minibufferp) default-color)
+                                     ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                                     ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                                     ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                                     (t default-color))))
+                    (set-face-background 'mode-line (car color))
+                    (set-face-foreground 'mode-line (cdr color))))))
     )
   )
