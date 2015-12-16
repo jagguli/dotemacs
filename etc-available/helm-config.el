7ff671cad035c5c83d4dcd450a8704a514c577f5
Steven Joseph <steven@stevenjoseph.in>
2015-12-16 11:17:25 +1100

    Reorganized helm sources

--- etc-available/helm-config.el
+++ etc-available/helm-config.el
@@ -49,11 +49,6 @@
    helm-ff-smart-completion t
    helm-ff-transformer-show-only-basename nil
    helm-findutils-skip-boring-files t
-   helm-for-files-preferred-list
-   (quote
-    (helm-source-buffers-list
-     helm-source-recentf helm-source-bookmarks
-     helm-source-file-cache helm-source-files-in-current-dir helm-source-locate))
    helm-mode-handle-completion-in-region t
    helm-mode-reverse-history nil
    helm-quick-update t
@@ -62,6 +57,27 @@
    helm-adaptive-mode t
    helm-full-frame nil
    projectile-completion-system 'helm
+   helm-buffer-max-length nil
+
+   helm-mini-default-sources
+   (quote
+    (
+     helm-source-projectile-buffers-list
+     helm-source-buffers-list
+     helm-source-recentf
+     helm-source-buffer-not-found
+     ))
+   helm-for-files-preferred-list
+   (quote
+    (helm-source-projectile-recentf-list
+     helm-source-recentf
+     helm-source-projectile-files-list
+     helm-source-files-in-current-dir
+     helm-source-file-cache
+     helm-source-bookmarks
+     helm-source-locate
+     )))
+
    ;;shackle-rules '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.5))
    ;;helm-split-window-default-side (quote left)
    ;;helm-adaptive-history-file
@@ -74,58 +90,17 @@
     (helm-projectile-on)
     
 
-    (defun helm-split-buffers-list ()
-      (interactive)
-      (let ((buffers (mapcar 'window-buffer (window-list))))
-        (if (= 1 (length buffers))
-            (split-window-sensibly)
-          (other-window 1)
-          ))
-      (helm-buffers-list))
     (global-set-key "\M-x" 'helm-M-x)
     (global-set-key "\C-x\C-r" 'helm-for-files)
     (global-set-key "\C-xb" 'helm-mini)
     (global-set-key "\C-x " 'helm-bookmarks)
     (global-set-key [(f3)] 'helm-split-buffers-list)
     (define-key helm-command-map "b" 'helm-bookmarks)
-    
-    ;;(global-set-key [(control f3)] (lambda ()
-    ;;                     (interactive) (ibuffer)))
-    ;;(global-set-key [(f3)] (lambda ()
-    ;;                          (interactive)
-    ;;			  ;;(split-window-right)
-    ;;                          (ibuffer t)
-    ;;                          (ibuffer-filter-disable)
-    ;;                          ))
-    (defun helm-eproject-locate ()
-      (interactive)
-      (helm-locate-with-db (format "%s/locatedb"
-                                   eproject-root))
-      )
-
-    (defun helm-eproject-ag ()
-      (interactive)
-      (let* ((helm-ag-default-directory (eproject-root))
-             (header-name (format "Search at %s" helm-ag-default-directory)))
-        (helm-attrset 'search-this-file nil helm-ag-source)
-        (helm-attrset 'name header-name helm-ag-source)
-        (helm :sources '(helm-ag-source) :buffer "*helm-ag*")))
     (global-set-key "\C-xt" 'helm-eproject-ag)
     (global-set-key "\C-xc" 'helm-resume)
     (global-set-key [(f5)] 'helm-etags-select)
     (global-set-key "\C-x/"  'helm-cmd-t)
     (global-set-key "\C-x\\"  'ag)
-    ;;(global-set-key "\C-x/"  'helm-eproject-locate)
-    ;;(global-set-key [(shift f3)] (lambda ()
-    ;;                               (Interactive) (ibuffer)))
-    ;;(global-set-key [(f3)] (lambda ()
-    ;;                          (interactive)
-    ;;                          ;;(evil-window-vsplit)
-    ;;                          (helm-mini)))
-    ;;(add-hook 'ido-make-buffer-list-hook
-    ;;          (lambda ()
-    ;;            (ibuffer-sort-bufferlist ido-temp-list) ))
-    ;;
     ;;(defun sort-buffers ()
     ;;  "Put the buffer list in alphabetical order."
     ;;  (interactive)
@@ -139,12 +114,14 @@
     ;;           (a b) (string<
     ;;                  (downcase (buffer-name a))
     ;;                  (downcase (buffer-name b)))))))
-
-
-
-
-    (helm-mode)
-    (print "loaded helm-config")
+    (defun helm-split-buffers-list ()
+      (interactive)
+      (let ((buffers (mapcar 'window-buffer (window-list))))
+        (if (= 1 (length buffers))
+            (split-window-sensibly)
+          (other-window 1)
+          ))
+      (helm-buffers-list))
     (defun helm-ag--query ()
       (let* ((searched-word (helm-ag--searched-word))
              (marked-word (helm-ag--marked-input))
@@ -152,6 +129,18 @@
         (when (string= query "")
           (error "Input is empty!!"))
         (setq helm-ag--last-query query)))
+    (helm-mode)
+    )
+  )
+
+(req-package helm-cmd-t
+  :config (concat user-lib-dir "helm-cmd-t")
+  :init
+  (progn 
+    (defun helm-cmd-t-ad-hoc-example ()
+      "Choose file from test folder."
+      (interactive)
+      (helm :sources (list downloads-source docs-source)))
     )
   )
 
@@ -166,13 +155,3 @@
 ;;                       ;;(global-set-key "\C-x?"  'helmrecoll)
 ;;                       )
 ;;             )
-(req-package helm-cmd-t
-             :config (concat user-lib-dir "helm-cmd-t")
-             :init (progn 
-
-    (defun helm-cmd-t-ad-hoc-example ()
-      "Choose file from test folder."
-      (interactive)
-      (helm :sources (list downloads-source docs-source)))
-    )
-             )
