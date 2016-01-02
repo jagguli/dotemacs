c353217c3f4ed84f5339eaba2732e0b3788c6538
Steven Joseph <steven@stevenjoseph.in>
2016-01-03 07:44:48 +1100

    use local proxy for twittering mode

--- etc-available/twittering-config.el
+++ etc-available/twittering-config.el
@@ -2,7 +2,7 @@
 (setq twittering-oauth-consumer-key "1csFkG3amwxR1M3ZWo2vA" )
 (setq twittering-oauth-consumer-secret "OVrqbE6Pghr9cnh2cuqIXjppe5pLE9FK1IwQIhEo")
 (setq twittering-proxy-use t)
-(setq twittering-https-proxy-server "sydxplanssh")
+(setq twittering-https-proxy-server "localhost")
 (setq twittering-https-proxy-port 3128)
 (setq twittering-use-master-password t)
 
