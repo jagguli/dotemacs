51d3074c47d84f9b98ad2215f2dceea23e7280ea
Steven Joseph <steven@stevenjoseph.in>
2018-09-02 21:10:30 +1000

    fix: flycheck for python

--- etc-available/flycheck-config.el
+++ etc-available/flycheck-config.el
@@ -1,11 +1,22 @@
-;; Enable flymake for all files
-;;(add-hook 'find-file-hook 'flycheck-mode-on)
-;; Enable flymake for Python only
-(add-hook 'python-mode-hook 'flycheck-mode-on-safe)
-(add-hook 'js2-mode-hook 'flycheck-mode-on-safe)
-(add-hook 'javascript-mode-hook 'flycheck-mode-on-safe)
-
-
-;;(setq flymake-no-changes-timeout 5)
-;;(add-hook 'evil-normal-state-entry-hook 'flymake-start-syntax-check)
-
+(use-package flycheck
+  :ensure t
+  :after pipenv
+  :init (progn
+          (global-flycheck-mode 1)
+          (defun pipenv-activate-pycheckers ()
+            "Activate integration of Pipenv with Flycheck."
+            (interactive)
+            (setq flycheck-pycheckers-venv-root  (pipenv-venv))
+            )
+          ;;(if (< (count-lines (point-min) (point-max)) 2000) 
+          ;;    (flycheck-mode) 
+          ;;  (flycheck-mode -1))
+          ;;(add-hook 'pipenv-mode-hook 'pipenv-activate-pycheckers)
+          ;;(add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
+          ;;(add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup))
+          ;;(add-hook 'python-mode-hook 'flycheck-mode-on-safe)
+          ;;(add-hook 'js2-mode-hook 'flycheck-mode-on-safe)
+          ;;(add-hook 'javascript-mode-hook 'flycheck-mode-on-safe)
+          ;;(add-hook 'evil-normal-state-entry-hook 'flymake-start-syntax-check)
+          )
+  )
