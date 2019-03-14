ee7b69339346fe333fcc69ccc6ba5332858b4c14
Steven Joseph <steven@stevenjoseph.in>
2019-03-15 01:30:00 +1100

    fix: add hydra config for orgmode

--- etc-available/hydra-config.el
+++ etc-available/hydra-config.el
@@ -48,6 +48,18 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
       ("`"   hydra-projectile-other-window/body "other window")
       ("q"   nil "cancel" :color blue))
 
-    (global-set-key (kbd "<f3>") 'hydra-projectile/body)
+    (defhydra hydra-global-org (:color blue)
+      "Org"
+      ("t" org-timer-start "Start Timer")
+      ("s" org-timer-stop "Stop Timer")
+      ("r" org-timer-set-timer "Set Timer") ; This one requires you be in an orgmode doc, as it sets the timer for the header
+      ("p" org-timer "Print Timer") ; output timer value to buffer
+      ("w" (org-clock-in '(4)) "Clock-In") ; used with (org-clock-persistence-insinuate) (setq org-clock-persist t)
+      ("o" org-clock-out "Clock-Out") ; you might also want (setq org-log-note-clock-out t)
+      ("j" org-clock-goto "Clock Goto") ; global visit the clocked task
+      ("c" org-capture "Capture") ; Don't forget to define the captures you want http://orgmode.org/manual/Capture.html
+      ("i" org-journal-new-entry "iJournal entry")
+      ("l" org-capture-goto-last-stored "Last Capture"))
+    (global-set-key (kbd "<f3>") 'hydra-global-org/body)
     )
   )
