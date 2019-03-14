7392337d27819d657404e869a7d4d00785e09692
Steven Joseph <steven@stevenjoseph.in>
2019-03-15 01:55:18 +1100

    feat: add org-gcal

---
+++ etc-available/gcal-config.el
@@ -0,0 +1,7 @@
+(req-package org-gcal
+  :config (
+           setq org-gcal-client-id "your-id-foo.apps.googleusercontent.com"
+                org-gcal-client-secret "your-secret"
+                org-gcal-file-alist '(("your-mail@gmail.com" .  "~/schedule.org")
+                                      ("another-mail@gmail.com" .  "~/task.org")))
+  )
