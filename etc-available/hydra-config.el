2dff60e18f1e9da6c17feda55ce95f79d1bb9d2c
Steven Joseph <steven@stevenjoseph.in>
2019-03-19 02:31:11 +1100

    fix: hydra for orgmode

--- etc-available/hydra-config.el
+++ etc-available/hydra-config.el
@@ -1,5 +1,9 @@
 ;;http://oremacs.com/2015/01/20/introducing-hydra/
 (req-package hydra
+  :require
+  (major-mode-hydra)
+  :bind
+  ("C-M-m" . major-mode-hydra)
   :init
   (progn
     (defhydra hydra-projectile-other-window (:color teal)
@@ -50,8 +54,8 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
 
     (defhydra hydra-global-org (:color blue)
       "Org"
-      ("t" org-timer-start "Start Timer")
-      ("s" org-timer-stop "Stop Timer")
+      ("s" org-timer-start "Start Timer")
+      ("S" org-timer-stop "Stop Timer")
       ("r" org-timer-set-timer "Set Timer") ; This one requires you be in an orgmode doc, as it sets the timer for the header
       ("p" org-timer "Print Timer") ; output timer value to buffer
       ("w" (org-clock-in '(4)) "Clock-In") ; used with (org-clock-persistence-insinuate) (setq org-clock-persist t)
@@ -59,7 +63,9 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
       ("j" org-clock-goto "Clock Goto") ; global visit the clocked task
       ("c" org-capture "Capture") ; Don't forget to define the captures you want http://orgmode.org/manual/Capture.html
       ("i" org-journal-new-entry "iJournal entry")
+      ("t" org-todo-list "List todos"))
       ("l" org-capture-goto-last-stored "Last Capture"))
     (global-set-key (kbd "<f3>") 'hydra-global-org/body)
+    (global-set-key (kbd "C-c p") 'hydra-projectile/body)
     )
   )
