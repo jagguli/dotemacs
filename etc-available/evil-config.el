8e9127ea2329390a2f44a5819e1d7c83b551b15f
Steven Joseph <steven@stevenjoseph.in>
2019-05-19 20:22:06 +1000

    feat: add evil-org

--- etc-available/evil-config.el
+++ etc-available/evil-config.el
@@ -131,3 +131,14 @@
     )
 
   )
+
+(req-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
