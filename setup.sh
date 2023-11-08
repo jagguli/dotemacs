872c5e1e4fee646cfbe4913f45ab8112c905e3a2
Steven Joseph <steven@stevenjoseph.in>
2023-11-08 23:49:53 +1100

    update setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ pip install --user --editable "git+https://github.com/jagguli/ropemacs.git#egg=r
 cd modules.d/Pymacs
 make
 python setup.py install --user
-cd ../..
+ln -sf ~/.emacs.d.jagguli/emacs-profiles.el .emacs-profiles.el
+cd ~/
+git clone https://github.com/plexus/chemacs2.git .emacs.d/
 
