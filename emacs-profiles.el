4c3345a2ee74f0d0f147edd5a9c01ee77b552bea
Steven Joseph <steven@stevenjoseph.in>
2023-11-08 23:49:53 +1100

    update setup

---
+++ emacs-profiles.el
@@ -0,0 +1,12 @@
+(("doomemacs"   . (
+		   (user-emacs-directory . "~/doom-emacs")
+		   (srver . t)
+		   (server-name . "doom")
+		   ))
+ ("legacy" . (
+	      (user-emacs-directory . "~/.emacs.d.jagguli")
+		   (server-name . "legacy")
+	      ))
+ ("spacemacs"   . ((user-emacs-directory . "~/spacemacs")
+		   (server-name . "spacemacs")
+		   )))
